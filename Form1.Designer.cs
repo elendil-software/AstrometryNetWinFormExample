2dd096952b2621afe39adaa2b5d28ee026cdd721
Julien Tschäppät <git@elendil.software>
2018-03-04 10:23:14 +0100

    WinForm example for AstrometryNetClient

---
+++ Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace TestWinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxApiKey = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxApiKey
+            // 
+            this.textBoxApiKey.Location = new System.Drawing.Point(64, 39);
+            this.textBoxApiKey.Name = "textBoxApiKey";
+            this.textBoxApiKey.Size = new System.Drawing.Size(153, 20);
+            this.textBoxApiKey.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(223, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 20);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Analyse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 65);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(286, 218);
+            this.textBoxLog.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "API Key";
+            // 
+            // textBoxFile
+            // 
+            this.textBoxFile.Location = new System.Drawing.Point(64, 10);
+            this.textBoxFile.Name = "textBoxFile";
+            this.textBoxFile.Size = new System.Drawing.Size(153, 20);
+            this.textBoxFile.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(310, 296);
+            this.Controls.Add(this.textBoxFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxApiKey);
+            this.Name = "Form1";
+            this.Text = "TestWinForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxApiKey;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using software.elendil.AstrometryNet;
+using software.elendil.AstrometryNet.Enum;
+using software.elendil.AstrometryNet.Json;
+
+namespace TestWinForms
+{
+
+    public struct Solution
+    {
+        public double RA;
+        public double Dec;
+        public double Radius;
+        public SolverStatus Status;
+    }
+
+    public enum SolverStatus
+    {
+        Connected,
+        NotConnected,
+        ImageSent,
+        Solving,
+        Success,
+        Failure,
+        Error,
+        Canceled
+    }
+
+    public partial class Form1 : Form
+    {
+        private Client client;
+        private string submissionId;
+        private SolverStatus status;
+        private string Message { get; set; }
+
+        private CancellationTokenSource cancellationTokenSource;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            status = SolverStatus.NotConnected;
+            Solve();
+        }
+
+        private async void Solve()
+        {
+            bool imageSent = await SendImage(textBoxFile.Text, cancellationTokenSource.Token);
+			if (!imageSent)
+			{
+                textBoxLog.Text += Message + Environment.NewLine;
+			}
+			else
+			{
+				cancellationTokenSource.Token.ThrowIfCancellationRequested();
+                textBoxLog.Text += @"Solving..." + Environment.NewLine;
+				Solution res = await GetSolution(cancellationTokenSource.Token);
+
+			    if (SolverStatus.Success.Equals(res.Status))
+			    {
+			        textBoxLog.Text += $@"Solution : RA : {res.RA} , Dec : {res.Dec} : {Environment.NewLine}";
+			    }
+			    else
+			    {
+			        textBoxLog.Text += Message + Environment.NewLine;
+			    }
+			}
+        }
+
+        private void Login()
+        {
+            if (status == SolverStatus.NotConnected)
+            {
+                client = new Client(textBoxApiKey.Text);
+                LoginResponse result = client.Login();
+
+                if (ResponseStatus.success.Equals(result.status))
+                {
+                    Message = result.message;
+                    status = SolverStatus.Connected;
+                }
+                else
+                {
+                    Message = result.errormessage;
+                    status = SolverStatus.NotConnected;
+                }
+            }
+            else
+            {
+                status = SolverStatus.Connected;
+            }
+        }
+
+        public async Task<bool> SendImage(string file, CancellationToken ct)
+        {
+            return await Task.Factory.StartNew(() =>
+            {
+                Login();
+
+                if (status != SolverStatus.Connected)
+                {
+                    return false;
+                }
+
+                submissionId = null;
+
+                UploadResponse result = client.Upload(file);
+
+                if (ResponseStatus.success.Equals(result.status))
+                {
+                    submissionId = result.subid;
+                    Message = file;
+                    status = SolverStatus.ImageSent;
+                    return true;
+                }
+
+                Message = result.errormessage;
+                return false;
+            }, ct);
+        }
+
+        /// <summary>
+		/// Get the solution (if solved)
+		/// </summary>
+		/// <returns></returns>
+		public async Task<Solution> GetSolution(CancellationToken ct)
+        {
+            var solution = new Solution();
+            var jobStatusResponse = new JobStatusResponse { status = ResponseJobStatus.failure };
+
+            var submissionStatusResponse = await client.GetSubmissionStatus(submissionId, ct);
+
+            if (submissionStatusResponse.jobs != null && !ct.IsCancellationRequested)
+            {
+                jobStatusResponse = await client.GetJobStatus(submissionStatusResponse.jobs[0], ct);
+            }
+
+
+            if (submissionStatusResponse.jobs != null && jobStatusResponse.status.Equals(ResponseJobStatus.success) &&
+                !ct.IsCancellationRequested)
+            {
+                CalibrationResponse calibrationResponse = client.GetCalibration(submissionStatusResponse.jobs[0]);
+
+                solution.RA = calibrationResponse.ra;
+                solution.Dec = calibrationResponse.dec;
+                solution.Radius = calibrationResponse.radius;
+                solution.Status = SolverStatus.Success;
+                return solution;
+            }
+
+            if (ct.IsCancellationRequested)
+            {
+                solution.Status = SolverStatus.Canceled;
+                return solution;
+            }
+
+            solution.Status = SolverStatus.Failure;
+            return solution;
+        }
+    }
+}
